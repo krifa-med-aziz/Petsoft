6abbab4215c257d9ed618b2e91e9498533b23745
Krifa Med Aziz <87083945+krifaaziz@users.noreply.github.com>
2025-09-11 16:45:59 +0100

    feat(stripe): update POST handler to process JSON data and fulfill order by granting user access

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,4 +1,19 @@
+import { prisma } from "../../../../prisma/prisma";
+
 export async function POST(request: Request) {
-  const body = await request.text();
-  console.log(body);
+  const data = await request.json();
+  // verify webhook came from Stripe
+
+  // fullfil order
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  });
+
+  // return 200 ok
+  return Response.json(null, { status: 200 });
 }
